4212ab1b8d744a75184369a5a61f5869b3219b72
David Buller <bullerd@comcast.net>
2025-12-29 15:03:57 -0500

    Change noteSchema's 'important' field validation format

--- backend/models/note.js
+++ backend/models/note.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const noteSchema = new mongoose.Schema({
   content: { type: String, minLength: 5, required: true },
-  important: Boolean,
+  important: { type: Boolean },
 });
 
 noteSchema.set("toJSON", {
